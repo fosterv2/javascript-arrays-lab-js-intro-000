349e3d9c933a7174acd60a6ca8762ab5c5a3a7bf
Valerie Foster <fosterv2@outlook.com>
2019-11-04 00:10:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,17 +18,17 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  
+
 }
 
 function prependKitten(name) {
-  
+
 }
 
 function removeLastKitten() {
-  
+
 }
 
 function removeFirstKitten() {
-  
+
 }
