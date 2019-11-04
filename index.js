0bda38399923481a2b8f289d2ab72e883102971d
Valerie Foster <fosterv2@outlook.com>
2019-11-04 00:10:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+
+function appendKitten(name) {
+  
+}
+
+function prependKitten(name) {
+  
+}
+
+function removeLastKitten() {
+  
+}
+
+function removeFirstKitten() {
+  
+}
