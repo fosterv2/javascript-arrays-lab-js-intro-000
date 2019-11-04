c2c896095b1784a02a45a18057d927e1e159dbbb
Valerie Foster <fosterv2@outlook.com>
2019-11-04 00:18:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,17 +18,18 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-
+  return kittens.concat(name)
 }
 
 function prependKitten(name) {
-
+  newArr = [name]
+  return name.concat(kittens)
 }
 
 function removeLastKitten() {
-
+  return kittens.slice(0, kittens.length - 1)
 }
 
 function removeFirstKitten() {
-
+  return kittens.slice(1)
 }
